da9c3d3598b222e8f43c081a902b421f3125a52a
lukasschulte03 <lukas007@live.se>
2022-06-28 15:37:58 +0200

    WIP

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,14 +1,19 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-canvas.width = window-innerWidth;
+canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+
+const connectThreshold = 200;
+const mouseRadiusDivider = 10;
+const speedMax = 2
 
 let particlesArray;
 
 let mouse = {
     x: null,
     y: null,
-    radius: (canvas.height/80) * (canvas.width/80)
+    radius: ((canvas.height/mouseRadiusDivider))
 }
 
 window.addEventListener('mousemove',
@@ -30,7 +35,7 @@ class Particle{
     draw(){
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI *2, false);
-        ctx.fillStyle = '#8C5523';
+        ctx.fillStyle = this.color;
         ctx.fill();
     }
     update(){
@@ -72,21 +77,57 @@ function init(){
         let size = (Math.random()*5) + 1;
         let x = (Math.random() * ((innerWidth - size *2) - (size*2)) + size*2);
         let y = (Math.random() * ((innerHeight - size *2) - (size*2)) + size*2);
-        let directionX = (Math.random() * 5) -2.5;
-        let directionY = (Math.random() * 5) -2.5;
-        let color = '#8C5523';
+        let directionX = Math.random() * (speedMax + speedMax) - speedMax;
+        let directionY = Math.random() * (speedMax + speedMax) - speedMax;
+        let color = '#ffffff';
 
         particlesArray.push(new Particle(x, y, directionX, directionY, size, color));
+    }
+}
+
+function connect(){
+    for (let a = 0; a < particlesArray.length; a++) {
+        let opacityValue = 1;
+        for (let b = a; b < particlesArray.length; b++) {
+            let distance = Math.sqrt(((particlesArray[a].x - particlesArray[b].x) * (particlesArray[a].x - particlesArray[b].x)) + ((particlesArray[a].y - particlesArray[b].y) * (particlesArray[a].y - particlesArray[b].y)))
+            if (distance < connectThreshold){
+                opacityValue = (connectThreshold - distance)/connectThreshold
+                ctx.strokeStyle = "rgba(255,255,255," + opacityValue + ")";
+                ctx.lineWidth = 1;
+                ctx.beginPath();
+                ctx.moveTo(particlesArray[a].x, particlesArray[a].y);
+                ctx.lineTo(particlesArray[b].x, particlesArray[b].y);
+                ctx.stroke();
+            }
+        }
     }
 }
 
 function animate(){
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, innerWidth, innerHeight);
-    for (let i = 0; i < array.length; i++) {
+    for (let i = 0; i < particlesArray.length; i++) {
         particlesArray[i].update();
     }
+    connect();
 }
+
+window.addEventListener("resize",
+    function(){
+        canvas.width = this.innerWidth;
+        canvas.height = this.innerHeight;
+        mouse.radius = ((canvas.height/mouseRadiusDivider))
+    }
+)
+
+window.addEventListener("mouseout",
+    function(){
+        mouse.x = undefined;
+        mouse.y = undefined;
+    }
+);
+
+
 
 init();
 animate();
